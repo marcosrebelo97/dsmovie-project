bf808621592b70ac008ad160d98ef775ae12bc24
marcosrebelo97 <mgr@icomp.ufam.edu.br>
2022-03-16 00:14:04 -0400

    Config Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+import {ReactComponent as GithubLogo} from 'assets/img/github.svg';
+import './styles.css';
+
+
+function Navbar(){
+    return (
+        <header>
+        <nav className='container'>
+          <div className='dsmovie-nav-content'>
+            <h1>DSMovie</h1>
+            <a href="https://github.com/marcosrebelo97/dsmovie-project">
+              <div className='dsmovie-contact-container'>
+                <GithubLogo />
+                <p className='dsmovie-contact-link'>marcosrebelo97</p>
+              </div>
+            </a>
+          </div>
+        </nav>
+      </header>
+    );
+}
+
+export default Navbar;
